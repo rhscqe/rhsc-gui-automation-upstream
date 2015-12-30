652d5a805b5a3fb6a1eec913c9fcfa34262bf327
Kasturi Narra <knarra@redhat.com>
2015-12-30 15:19:33 +0530

    adding fingerprint for rhel6 nodes

--- src/com/redhat/qe/storageconsole/sahi/tasks/StorageSahiImportTasks.java
+++ src/com/redhat/qe/storageconsole/sahi/tasks/StorageSahiImportTasks.java
@@ -490,7 +490,8 @@ public class StorageSahiImportTasks {
 		// Example: "stderr:::stdout:2048 2b:11:18:7b:79:8e:04:f3:b8:48:6c:2b:bb:4e:62:84 /etc/ssh/ssh_host_rsa_key.pub (RSA)"
 		
 		commandOutput = commandOutput.substring(commandOutput.indexOf("stdout:"));
-		fingerprint = commandOutput.substring(commandOutput.indexOf(' '), commandOutput.lastIndexOf(" "));
+		fingerprint = commandOutput.substring(commandOutput.indexOf(' '), commandOutput.lastIndexOf(" /"));
+		//fingerprint = commandOutput.substring(commandOutput.indexOf(' '), commandOutput.lastIndexOf(" "));
 		return fingerprint.trim();
 	}
 	
